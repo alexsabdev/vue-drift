f4fe0308399108be57fc6b813a539d9032be59ea
alex sab <alexsabdev@gmail.com>
2019-09-18 13:34:45 +0300

    + script minify (v1.1.1)

--- src/index.js
+++ src/index.js
@@ -15,28 +15,7 @@ function loadInitScript(appId) {
   if (installed) {
     return;
   }
-  const content = `!function() {
-    var t = window.driftt = window.drift = window.driftt || [];
-    if (!t.init) {
-      if (t.invoked) return void (window.console && console.error && console.error("Drift snippet included twice."));
-      t.invoked = !0, t.methods = [ "identify", "config", "track", "reset", "debug", "show", "ping", "page", "hide", "off", "on" ],
-      t.factory = function(e) {
-        return function() {
-          var n = Array.prototype.slice.call(arguments);
-          return n.unshift(e), t.push(n), t;
-        };
-      }, t.methods.forEach(function(e) {
-        t[e] = t.factory(e);
-      }), t.load = function(t) {
-        var e = 3e5, n = Math.ceil(new Date() / e) * e, o = document.createElement("script");
-        o.type = "text/javascript", o.async = !0, o.crossorigin = "anonymous", o.src = "https://js.driftt.com/include/" + n + "/" + t + ".js";
-        var i = document.getElementsByTagName("script")[0];
-        i.parentNode.insertBefore(o, i);
-      };
-    }
-  }();
-  drift.SNIPPET_VERSION = '0.3.1';
-  drift.load('${appId}');`;
+  const content = `!function(){var t=window.driftt=window.drift=window.driftt||[];if(!t.init){if(t.invoked)return void(window.console&&console.error&&console.error("Drift snippet included twice."));t.invoked=!0,t.methods=["identify","config","track","reset","debug","show","ping","page","hide","off","on"],t.factory=function(e){return function(){var n=Array.prototype.slice.call(arguments);return n.unshift(e),t.push(n),t}},t.methods.forEach(function(e){t[e]=t.factory(e)}),t.load=function(t){var e=3e5*Math.ceil(new Date/3e5),n=document.createElement("script");n.type="text/javascript",n.async=!0,n.crossorigin="anonymous",n.src="https://js.driftt.com/include/"+e+"/"+t+".js";var o=document.getElementsByTagName("script")[0];o.parentNode.insertBefore(n,o)}}}(),drift.SNIPPET_VERSION="0.3.1",drift.load("${appId}");`;
   appendScript(content);
   installed = true;
 }
@@ -45,9 +24,7 @@ function loadIdentifyScript(userId, attributes = {}) {
   if (userId === undefined) {
     throw new Error('[vue-drift] missing the "userId" parameter');
   }
-  const scriptText = `!function () {
-    drift.identify('${userId}', ${JSON.stringify(attributes)})
-  }(drift);`;
+  const scriptText = `!function(){drift.identify('${userId}',${JSON.stringify(attributes)})}(drift);`;
   appendScript(scriptText);
 }
 
